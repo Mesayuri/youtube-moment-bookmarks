c6e997326f3dd4d8d8b410ef625a168e4341b61f
Mesayuri <beachsidebunny653@gmail.com>
2020-11-25 12:26:03 +0900

    Add API of Tags

--- frontend/src/api/bookmark.ts
+++ frontend/src/api/bookmark.ts
@@ -33,3 +33,35 @@ export const fetchBookmarks = async (tagIdList: number[]) => {
   const data = await response.json();
   return data;
 };
+
+export const sendNewBookmark = async (newBookmark: NewBookmark) => {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(newBookmark),
+  };
+
+  const endpoint = `${backend_url}/create/bookmark`;
+  const response = await fetch(endpoint, requestOptions);
+  const data = await response.json();
+
+  return data.result;
+};
+
+export const sendBookmarkUpdate = async (bookmark: Bookmark) => {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(bookmark),
+  };
+
+  const endpoint = `${backend_url}/create/bookmark`;
+  const response = await fetch(endpoint, requestOptions);
+  const data = await response.json();
+
+  return data.result;
+};
---
+++ frontend/src/api/tag.ts
@@ -0,0 +1,51 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const backend_url = process.env.BACKEND_URL || 'localhost:8080';
+
+export type Tag = {
+  id: number,
+  tagName: string,
+  color: string,
+};
+
+export type NewTag = {
+  tagName: string,
+  color: string,
+};
+
+export const fetchTagList = async () => {
+  const response = await fetch(`${backend_url}/list/tag`);
+  const data = await response.json();
+  return data;
+};
+
+export const sendNewTag = async (newTag: NewTag) => {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(newTag),
+  };
+
+  const response = await fetch(`${backend_url}/create/tag`, requestOptions);
+  const data = await response.json();
+
+  return data.result;
+};
+
+export const sendTagUpdate = async (tag: Tag) => {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(tag),
+  };
+
+  const response = await fetch(`${backend_url}/update/tag`, requestOptions);
+  const data = await response.json();
+
+  return data.result;
+};
